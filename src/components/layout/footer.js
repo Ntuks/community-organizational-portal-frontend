fab4fc3cf01f03569fdf97dffa67733d71f1503c
Akhil Boddu <akhil1234mara@gmail.com>
2019-08-03 18:28:59 +0200

    Modified Headers & Footers

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -3,7 +3,6 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import Link from '@material-ui/core/Link';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -27,7 +26,7 @@ export default function Footer() {
       <footer className={classes.footer}>
         <Container maxWidth="lg">
           <Typography variant="body1">Community Organization Portal</Typography>
-          <Typography variant="body1">Changing lives. One step at a time. Together</Typography>
+          <Typography variant="body2" color="textSecondary">Changing lives. One step at a time. Together</Typography>
         </Container>
       </footer>
     </div>
--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -4,8 +4,6 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -24,13 +22,12 @@ function Header(props) {
     return (
         <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
+          
           <Typography variant="h6" className={classes.title}>
             {props.title}
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button color="inherit">Organization Sign Up</Button>
+          <Button color="inherit">Organization Sign In</Button>
         </Toolbar>
       </AppBar>
     )
